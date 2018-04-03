c6d3a6197a8eadc98e18b826137d4c43ae5f5f81
Dreik3 <nik.dolmatov@gmail.com>
2018-04-04 02:43:30 +0300

    added few types of inputs for options box, added factory to share data from optBox controller to charts-item component, logic still not work, have to figure it out tomorrow

--- app/app.js
+++ app/app.js
@@ -2,3 +2,103 @@
 
 // Declare app level module which depends on views, and components
 let myApp = angular.module('myApp',  ["highcharts-ng","ntCharts"]);
+
+myApp.controller('optBoxCtrl', function($scope, storageForData) {
+    $scope.isBoxOpen = false;
+    //get random arrays for charts data - will be changed for some public api data later
+    let getRndDataArr = function() {
+        let rndDataArr = [];
+        for (let i = 0; i < 5; i++) {
+            rndDataArr[i] = Math.floor((Math.random() * 100) + 1);
+        }
+        return rndDataArr;
+    };
+    let chartConfig = {
+        chart1: {
+            chart: {
+                type: 'bar'
+            },
+            series: [{
+                data: getRndDataArr(),
+                id: 'series1'
+            }],
+            title: {
+                text: 'chart1'
+            },
+            value: true/*,
+            color: "#4088ED"*/
+        },
+        chart2: {
+            chart: {
+                type: 'bar'
+            },
+            series: [{
+                data: getRndDataArr(),
+                id: 'series2'
+            }],
+            title: {
+                text: 'chart2'
+            },
+            value: true/*,
+            color: "#4088ED"*/
+        },
+        chart3: {
+            chart: {
+                type: 'bar'
+            },
+            series: [{
+                data: getRndDataArr(),
+                id: 'series3'
+            }],
+            title: {
+                text: 'chart3'
+            },
+            value: true/*,
+            color: "#4088ED"*/
+        },
+        chart4: {
+            chart: {
+                type: 'bar'
+            },
+            series: [{
+                data: getRndDataArr(),
+                id: 'series4'
+            }],
+            title: {
+                text: 'chart4'
+            },
+            value: true/*,
+            color: "#4088ED"*/
+        }
+    };
+    console.log(chartConfig.chart1.value, chartConfig.length);
+    let chartConfigEdit = {};
+    let chartsNumber = 4;
+    //updating our chartConfig object, we shouldnt show charts if checkbox on them is false
+    for (let i = 0; i < chartsNumber; i++) {
+        if (chartConfig.chart1.value) {
+            chartConfigEdit.chart1 = chartConfig.chart1;
+        }
+        if (chartConfig.chart2.value) {
+            chartConfigEdit.chart2 = chartConfig.chart2;
+        }
+        if (chartConfig.chart3.value) {
+            chartConfigEdit.chart3 = chartConfig.chart3;
+        }
+        if (chartConfig.chart4.value) {
+            chartConfigEdit.chart4 = chartConfig.chart4;
+        }
+    }
+    storageForData.getData(chartConfigEdit);
+});
+//factory used for sharing data from optBoxCtrl to chart-item component
+myApp.factory('storageForData', function(){
+    let model = {};
+    return {
+        model,
+        getData: function (chartConfig) {
+            model.chartConfig = chartConfig;
+        }
+    }
+
+});
--- app/components/charts/charts-item/charts-item.component.js
+++ app/components/charts/charts-item/charts-item.component.js
@@ -5,63 +5,16 @@ angular
     .module('ntCharts.charts-item').component('chartsItem', {
     templateUrl: 'components/charts/charts-item/charts-item.tmpl.html',
     controller: ChartsItemController,
+    //СПРОСИ У ЕГОРА ЧТО ЭТО ЗА ХНЯ И КАК ЕЕ ИСПОЛЬЗОВАТЬ
     resolve: {
         chart:'>'
     }
 });
 
-function ChartsItemController($scope, $element, $attrs) {
-    let ctrl = this;
-    $scope.chartConfig = {
-        chart1: {
-            chart: {
-                type: 'bar'
-            },
-            series: [{
-                data: [10, 15, 12, 8, 7],
-                id: 'series1'
-            }],
-            title: {
-                text: 'chart1'
-            }
-        },
-        chart2: {
-            chart: {
-                 type: 'bar'
-            },
-            series: [{
-                 data: [15, 3, 2, 12, 12],
-                 id: 'series2'
-            }],
-            title: {
-                text: 'chart2'
-            }
-        },
-        chart3: {
-            chart: {
-                type: 'bar'
-            },
-            series: [{
-                data: [2, 5, 12, 18, 17],
-                id: 'series3'
-            }],
-            title: {
-                text: 'chart3'
-            }
-        },
-        chart4: {
-            chart: {
-                 type: 'bar'
-            },
-            series: [{
-                 data: [15, 13, 21, 12, 22],
-                 id: 'series4'
-            }],
-            title: {
-                text: 'chart4'
-            }
-        }
-    }
+function ChartsItemController($scope, storageForData/*$element, $attrs*/) {
+    $scope.chartConfig = storageForData.model.chartConfig;
+    console.log("test", $scope.chartConfig);
+
 }
 
 
